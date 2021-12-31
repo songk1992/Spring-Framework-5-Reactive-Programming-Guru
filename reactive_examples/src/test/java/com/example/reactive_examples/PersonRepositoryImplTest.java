8268d0dd551228fcc0ca4d274bf28b4c1b45dcf1
songk1992-home <zuppiy@naver.com>
2021-12-31 22:28:40 +0900

    Flux Operations

--- reactive_examples/src/test/java/com/example/reactive_examples/PersonRepositoryImplTest.java
+++ reactive_examples/src/test/java/com/example/reactive_examples/PersonRepositoryImplTest.java
@@ -3,9 +3,10 @@ package com.example.reactive_examples;
 import com.example.reactive_examples.domain.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 class PersonRepositoryImplTest {
 
@@ -27,20 +28,42 @@ class PersonRepositoryImplTest {
     }
 
     @Test
-    void getByIdSubscribe(){
+    void getByIdSubscribe() {
         Mono<Person> personMono = personRepository.getById(1);
 
         personMono.subscribe(person -> System.out.println(person.toString()));
     }
 
     @Test
-    void getByIdMapFunction(){
+    void getByIdMapFunction() {
         Mono<Person> personMono = personRepository.getById(1);
 
         personMono.map(person -> {
             System.out.println(person.toString());
             return person.getFirstName();
         }).subscribe(firstName -> System.out.println("from map : " + firstName));
+    }
 
+    @Test
+    void fluxTestBlockFirst() {
+        Flux<Person> personFlux = personRepository.findAll();
+        Person person = personFlux.blockFirst();
+        assert person != null;
+        System.out.println(person);
+    }
+
+    @Test
+    void testFluxSubscribe() {
+        Flux<Person> personFlux = personRepository.findAll();
+        personFlux.subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxToListMono() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Mono<List<Person>> personListMono = personFlux.collectList();
+
+        personListMono.subscribe(list -> list.forEach(System.out::println));
     }
 }
