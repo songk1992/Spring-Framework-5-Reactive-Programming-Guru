3050e2f4d71ca5781195081a8df6de3f85b0d6ac
songk1992-home <zuppiy@naver.com>
2021-12-31 22:04:37 +0900

    Implement Reactive Repository

--- reactive_examples/src/main/java/com/example/reactive_examples/PersonRepositoryImpl.java
+++ reactive_examples/src/main/java/com/example/reactive_examples/PersonRepositoryImpl.java
@@ -5,13 +5,19 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class PersonRepositoryImpl implements PersonRepository{
+
+    Person kimc = new Person(1, "C", "Kim");
+    Person kimd = new Person(2, "d", "Kim");
+    Person kime = new Person(3, "e", "Kim");
+    Person kimf = new Person(4, "f", "Kim");
+
     @Override
     public Mono<Person> getById(Integer id) {
-        return null;
+        return Mono.just(kimc);
     }
 
     @Override
     public Flux<Person> findAll() {
-        return null;
+        return Flux.just(kimc, kimd, kime, kimf);
     }
 }
