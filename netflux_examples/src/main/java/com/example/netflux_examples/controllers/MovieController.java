de6bb973df39baec2fa18dbea9168cccf61b3855
songk1992-home <zuppiy@naver.com>
2022-01-02 23:35:23 +0900

    Create Spring MVC Controller

---
+++ netflux_examples/src/main/java/com/example/netflux_examples/controllers/MovieController.java
@@ -0,0 +1,29 @@
+package com.example.netflux_examples.controllers;
+
+import com.example.netflux_examples.domain.Movie;
+import com.example.netflux_examples.services.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/movies")
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @GetMapping("/{id}")
+    Mono<Movie> getMovieById(@PathVariable String id) {
+        return movieService.getMovieById(id);
+    }
+
+    @GetMapping
+    Flux<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+}
