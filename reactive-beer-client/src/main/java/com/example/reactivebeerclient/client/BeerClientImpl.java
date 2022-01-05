45cd63dc8911c506667614ca3797a8716991fc6b
songk1992-home <zuppiy@naver.com>
2022-01-05 23:38:50 +0900

    Using URL Parameters

--- reactive-beer-client/src/main/java/com/example/reactivebeerclient/client/BeerClientImpl.java
+++ reactive-beer-client/src/main/java/com/example/reactivebeerclient/client/BeerClientImpl.java
@@ -35,17 +35,17 @@ public class BeerClientImpl implements BeerClient {
 
     @Override
     public Mono<BeerDto> getBeerById(UUID id, Boolean showInventoryOnHand) {
-        return webClient.get().uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_PATH + "/" + id.toString())
+        return webClient.get().uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_PATH_GET_BY_ID)
                 .queryParamIfPresent("showInventoryOnHand", Optional.ofNullable(showInventoryOnHand))
-                .build())
+                .build(id))
                 .retrieve()
                 .bodyToMono(BeerDto.class);
     }
 
     @Override
     public Mono<BeerDto> getBeerByUPC(String upc) {
-        return webClient.get().uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_UPC_PATH + "/" + upc)
-                        .build())
+        return webClient.get().uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_UPC_PATH)
+                        .build(upc))
                 .retrieve()
                 .bodyToMono(BeerDto.class);
     }
