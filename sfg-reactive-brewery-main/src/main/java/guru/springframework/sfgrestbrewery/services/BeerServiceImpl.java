724f1799a089ea9a1118f3e240a1b1c0a05b8ce9
songk1992-home <zuppiy@naver.com>
2022-01-11 00:26:37 +0900

    Maven Dependencies

--- sfg-reactive-brewery-main/src/main/java/guru/springframework/sfgrestbrewery/services/BeerServiceImpl.java
+++ sfg-reactive-brewery-main/src/main/java/guru/springframework/sfgrestbrewery/services/BeerServiceImpl.java
@@ -32,43 +32,43 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
 
-        BeerPagedList beerPagedList;
+        BeerPagedList beerPagedList = null;
         Page<Beer> beerPage;
 
-        if (!StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
-            //search both
-            beerPage = beerRepository.findAllByBeerNameAndBeerStyle(beerName, beerStyle, pageRequest);
-        } else if (!StringUtils.isEmpty(beerName) && StringUtils.isEmpty(beerStyle)) {
-            //search beer_service name
-            beerPage = beerRepository.findAllByBeerName(beerName, pageRequest);
-        } else if (StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
-            //search beer_service style
-            beerPage = beerRepository.findAllByBeerStyle(beerStyle, pageRequest);
-        } else {
-            beerPage = beerRepository.findAll(pageRequest);
-        }
-
-        if (showInventoryOnHand){
-            beerPagedList = new BeerPagedList(beerPage
-                    .getContent()
-                    .stream()
-                    .map(beerMapper::beerToBeerDtoWithInventory)
-                    .collect(Collectors.toList()),
-                    PageRequest
-                            .of(beerPage.getPageable().getPageNumber(),
-                                    beerPage.getPageable().getPageSize()),
-                    beerPage.getTotalElements());
-        } else {
-            beerPagedList = new BeerPagedList(beerPage
-                    .getContent()
-                    .stream()
-                    .map(beerMapper::beerToBeerDto)
-                    .collect(Collectors.toList()),
-                    PageRequest
-                            .of(beerPage.getPageable().getPageNumber(),
-                                    beerPage.getPageable().getPageSize()),
-                    beerPage.getTotalElements());
-        }
+//        if (!StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
+//            //search both
+//            beerPage = beerRepository.findAllByBeerNameAndBeerStyle(beerName, beerStyle, pageRequest);
+//        } else if (!StringUtils.isEmpty(beerName) && StringUtils.isEmpty(beerStyle)) {
+//            //search beer_service name
+//            beerPage = beerRepository.findAllByBeerName(beerName, pageRequest);
+//        } else if (StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
+//            //search beer_service style
+//            beerPage = beerRepository.findAllByBeerStyle(beerStyle, pageRequest);
+//        } else {
+//            beerPage = beerRepository.findAll(pageRequest);
+//        }
+//
+//        if (showInventoryOnHand){
+//            beerPagedList = new BeerPagedList(beerPage
+//                    .getContent()
+//                    .stream()
+//                    .map(beerMapper::beerToBeerDtoWithInventory)
+//                    .collect(Collectors.toList()),
+//                    PageRequest
+//                            .of(beerPage.getPageable().getPageNumber(),
+//                                    beerPage.getPageable().getPageSize()),
+//                    beerPage.getTotalElements());
+//        } else {
+//            beerPagedList = new BeerPagedList(beerPage
+//                    .getContent()
+//                    .stream()
+//                    .map(beerMapper::beerToBeerDto)
+//                    .collect(Collectors.toList()),
+//                    PageRequest
+//                            .of(beerPage.getPageable().getPageNumber(),
+//                                    beerPage.getPageable().getPageSize()),
+//                    beerPage.getTotalElements());
+//        }
 
         return beerPagedList;
     }
@@ -76,32 +76,33 @@ public class BeerServiceImpl implements BeerService {
     @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false ")
     @Override
     public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
-        if (showInventoryOnHand) {
-            return beerMapper.beerToBeerDtoWithInventory(
-                    beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
-            );
-        } else {
-            return beerMapper.beerToBeerDto(
-                    beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
-            );
-        }
+//        if (showInventoryOnHand) {
+//            return beerMapper.beerToBeerDtoWithInventory(
+//                    beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
+//            );
+//        } else {
+//            return beerMapper.beerToBeerDto(
+//                    beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
+//            );
+//        }
+        return null;
     }
 
     @Override
     public BeerDto saveNewBeer(BeerDto beerDto) {
-        return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
+        return null;//beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
     }
 
     @Override
     public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
-        Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+//        Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+//
+//        beer.setBeerName(beerDto.getBeerName());
+//        beer.setBeerStyle(BeerStyleEnum.PILSNER.valueOf(beerDto.getBeerStyle()));
+//        beer.setPrice(beerDto.getPrice());
+//        beer.setUpc(beerDto.getUpc());
 
-        beer.setBeerName(beerDto.getBeerName());
-        beer.setBeerStyle(BeerStyleEnum.PILSNER.valueOf(beerDto.getBeerStyle()));
-        beer.setPrice(beerDto.getPrice());
-        beer.setUpc(beerDto.getUpc());
-
-        return beerMapper.beerToBeerDto(beerRepository.save(beer));
+        return null;//beerMapper.beerToBeerDto(beerRepository.save(beer));
     }
 
     @Cacheable(cacheNames = "beerUpcCache")
